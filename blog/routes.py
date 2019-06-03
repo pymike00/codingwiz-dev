5b9796d5b68c7ae5eef479e2c05ce96b39382b71
pymike00 <pymike00@gmail.com>
2019-06-03 17:11:35 +0200

    Post Detail Route, View and Template

--- blog/routes.py
+++ blog/routes.py
@@ -1,4 +1,5 @@
 from flask import render_template
+
 from blog import app
 from blog.models import Post
 
@@ -7,6 +8,12 @@ from blog.models import Post
 def homepage():
     posts = Post.query.order_by(Post.created_at.desc()).all()
     return render_template("homepage.html", posts=posts)
+
+
+@app.route("/posts/<int:post_id>")
+def post_detail(post_id):
+    post_instance = Post.query.get_or_404(post_id)
+    return render_template("post_detail.html", post=post_instance)
 
 
 @app.route("/about")
