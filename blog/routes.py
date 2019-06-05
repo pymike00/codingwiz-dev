cb86bf437f0bfb55d38941206443198c33cb274a
pymike00 <pymike00@gmail.com>
2019-06-05 19:46:48 +0200

    Post Update Functionality

--- blog/routes.py
+++ blog/routes.py
@@ -1,4 +1,4 @@
-from flask import flash, redirect, render_template, url_for
+from flask import abort, flash, redirect, render_template, request, url_for
 from flask_login import current_user, login_user, logout_user, login_required
 
 from blog import db, app
@@ -28,6 +28,26 @@ def post_create():
         db.session.add(new_post)
         db.session.commit()
         return redirect(url_for('post_detail', post_id=new_post.id))
+    return render_template("post_editor.html", form=form)
+
+
+@app.route("/posts/<int:post_id>/update", methods=["GET", "POST"])
+@login_required
+def post_update(post_id):
+    post_instance = Post.query.get_or_404(post_id)
+    if post_instance.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post_instance.title = form.title.data
+        post_instance.description = form.description.data
+        post_instance.body = form.body.data
+        db.session.commit()
+        return redirect(url_for('post_detail', post_id=post_instance.id))
+    elif request.method == "GET":
+        form.title.data = post_instance.title
+        form.description.data = post_instance.description
+        form.body.data = post_instance.body
     return render_template("post_editor.html", form=form)
 
 
