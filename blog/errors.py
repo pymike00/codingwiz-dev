0f6fe9b207118e5cabec8b0cbcaf607ae709e104
pymike00 <pymike00@gmail.com>
2019-06-08 17:04:08 +0200

    Naming Fix

--- blog/errors.py
+++ blog/errors.py
@@ -6,5 +6,5 @@ def not_found_error(error):
     return render_template("404.html"), 404
 
 @app.errorhandler(401)
-def not_found_error(error):
+def unauthorized_error(error):
     return render_template("401.html"), 401
